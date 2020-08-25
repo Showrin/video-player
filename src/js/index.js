8d7b70ead1992b4311c06dee3fceec494b5b810a
Showrin <showrinece15@gmail.com>
2020-08-25 10:57:39 +0600

    Add play and pause features

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+import Player from "./player/Player.js";
+var videoPlayerSource = document.getElementById("js-video-player");
+var playPauseButton = document.getElementById("js-play-pause-button");
+var rewindButton = document.getElementById("js-rewind-button");
+var forwardButton = document.getElementById("js-forwrd-button");
+var player = new Player();
+var videoPlayer = player.getVideoPlayer(videoPlayerSource);
+var togglePlayButtonIcon = function (isPlaying) {
+    console.log(isPlaying);
+    if (isPlaying) {
+        playPauseButton.src = "./src/assets/icons/pause.svg";
+    }
+    else {
+        playPauseButton.src = "./src/assets/icons/play.svg";
+    }
+};
+playPauseButton.addEventListener("click", function () {
+    if (videoPlayer.isPlaying) {
+        videoPlayer.pause();
+    }
+    else {
+        videoPlayer.play();
+    }
+    togglePlayButtonIcon(videoPlayer.isPlaying);
+});
---
+++ src/js/index.ts
@@ -0,0 +1,28 @@
+import Player from "./player/Player.js";
+
+const videoPlayerSource: any = document.getElementById("js-video-player");
+const playPauseButton: any = document.getElementById("js-play-pause-button");
+const rewindButton: any = document.getElementById("js-rewind-button");
+const forwardButton: any = document.getElementById("js-forwrd-button");
+
+const player = new Player();
+const videoPlayer = player.getVideoPlayer(videoPlayerSource);
+
+const togglePlayButtonIcon = (isPlaying: boolean) => {
+  console.log(isPlaying);
+  if (isPlaying) {
+    playPauseButton.src = "./src/assets/icons/pause.svg";
+  } else {
+    playPauseButton.src = "./src/assets/icons/play.svg";
+  }
+};
+
+playPauseButton.addEventListener("click", () => {
+  if (videoPlayer.isPlaying) {
+    videoPlayer.pause();
+  } else {
+    videoPlayer.play();
+  }
+
+  togglePlayButtonIcon(videoPlayer.isPlaying);
+});

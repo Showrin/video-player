ef3100af1a658ca4a01856dc3a615a893981c5ad
Showrin <showrinece15@gmail.com>
2020-08-25 18:15:07 +0600

    Write tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,46 @@
+import Player from "../src/js/player/Player.js";
+
+const player = new Player();
+const videoPlayer = player.getVideoPlayer();
+
+describe("index.html", () => {
+  it("should call play() function", () => {
+    videoPlayer.play = jest.fn();
+
+    videoPlayer.play();
+
+    expect(videoPlayer.play).toHaveBeenCalled();
+  });
+
+  it("should call pause() function", () => {
+    videoPlayer.pause = jest.fn();
+
+    videoPlayer.pause();
+
+    expect(videoPlayer.pause).toHaveBeenCalled();
+  });
+
+  it("should call forward() function", () => {
+    videoPlayer.forward = jest.fn();
+
+    videoPlayer.forward();
+
+    expect(videoPlayer.forward).toHaveBeenCalled();
+  });
+
+  it("should call rewind() function", () => {
+    videoPlayer.rewind = jest.fn();
+
+    videoPlayer.rewind();
+
+    expect(videoPlayer.rewind).toHaveBeenCalled();
+  });
+
+  it("should call played() function", () => {
+    videoPlayer.played = jest.fn();
+
+    videoPlayer.played();
+
+    expect(videoPlayer.played).toHaveBeenCalled();
+  });
+});
